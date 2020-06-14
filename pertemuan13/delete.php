80c04d66b72a72138bd66f600ffb84a43a138433
Hesti Rahmawati <62061503+HestiRahmawati20@users.noreply.github.com>
2020-06-14 09:45:31 +0700

    elearning13

---
+++ pertemuan13/delete.php
@@ -0,0 +1,12 @@
+<?php  
+include "koneksi.php";
+$articleID = $_GET['articleID'];
+$perintah = "DELETE FROM articles WHERE articleID =\"$articleID\"";
+if (mysqli_query($koneksi, $perintah)) {
+    echo "Artikel berhasil dihapus<br>";
+    echo "<a href=\"Tampilan.php\">Back</a>";
+} else {
+    echo "Komentar gagal dihapus. Kemungkinan terjadi kegagalan koneksi
+ ke database MySQL.";
+}
+?>
